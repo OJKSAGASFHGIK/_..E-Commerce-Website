44bb25202bf727758a760ee0eca5cc80bb3d506a
Greque <ichijou_raku@hotmail.com>
2025-06-30 21:13:17 -0300

    Server

---
+++ api/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+const PORT = process.env.PORT;
+// test route
+app.get("/", (req, res) => {
+    res.send("App is running");
+});
+
+app.listen(PORT || 9000, ()=>{
+    console.log(`Server listening on port ${PORT}`)
+})
