44181b434712b42cc85391f624986fde8257635a
Greque <ichijou_raku@hotmail.com>
2025-07-17 10:24:48 -0300

    token and middleware

--- api/models/User.js
+++ api/models/User.js
@@ -12,10 +12,18 @@ const UserSchema = new mongoose.Schema(
         }
     }, { timestamps: true })
 
-    // validade password match or not
-    UserSchema.methods.matchPassword = async function(enterPassword){
-        return await bcrypt.compare(enterPassword, this.password);
-    };
+// validade password match or not
+UserSchema.methods.matchPassword = async function(enterPassword){
+    return await bcrypt.compare(enterPassword, this.password);
+};
 
+// register password hash and store
+UserSchema.pre(
+    "save", async function(next){
+    if (!this.isModified("password")) { next(); };
+    
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+});
 
-    module.exports = mongoose.model("User", UserSchema);
+module.exports = mongoose.model("User", UserSchema);
--- api/routes/User.js
+++ api/routes/User.js
@@ -2,6 +2,8 @@ const express = require("express");
 const UserRoute = express.Router();
 const asyncHandler = require("express-async-handler");
 const User = require("../models/User");
+const GenerateToken = require("../tokenGenerator");
+const AuthenticantionProtect = require("../middleware/Auth");
 
 UserRoute.post(
     "/login",
@@ -14,12 +16,57 @@ UserRoute.post(
                 name: user.name,
                 email: user.email,
                 isAdmin: user.isAdmin,
-                token: null,
+                token: GenerateToken(user._id),
                 createdAt: user.createdAt,
             });
         } else {
             res.status(401);
-            throw new Error("Invalid Email or Password. ");
+            throw new Error("Invalid Email or Password.");
+        }
+    })
+);
+
+// register route
+UserRoute.post("/", asyncHandler(async(req, res) => {
+    const { name, email, password } = req.body;
+    const existUser = await User.findOne({ email });
+    if (existUser){
+        res.status(400);
+        throw new Error("User already exist.");
+    } else {
+        const user = await User.create({ name, email, password });
+        if (user){
+            res.status(201).json({
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+                isAdmin: user.isAdmin,
+                createdAt: user.createdAt,
+            });
+        } else {
+            res.status(400);
+            throw new Error("Invalid user data.");
+        }    
+    }
+}));
+
+// get auth profile data
+UserRoute.get(
+    "/profile",
+    AuthenticantionProtect,
+    asyncHandler(async(req, res) => {
+        const user = await User.findById(req.user._id);
+        if (user){
+            res.json({
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+                isAdmin: user.isAdmin,
+                createdAt: user.createdAt,
+            });
+        } else {
+            res.status(404);
+            throw new Error("USE NOT FOUND.")
         }
     })
 );
