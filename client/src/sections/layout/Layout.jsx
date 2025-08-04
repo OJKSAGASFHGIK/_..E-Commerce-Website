d19e982608c3615cd63c1f3d0e2b42af185210ab
Greque <ichijou_raku@hotmail.com>
2025-08-04 18:30:41 -0300

    client

---
+++ client/src/sections/layout/Layout.jsx
@@ -0,0 +1,12 @@
+import Navbar from './Navbar.jsx';
+import Footer from './Footer.jsx';
+
+const Layout = ({children}) => {return(
+    <>
+        <Navbar/>
+        <main>{children}</main>
+        <Footer/>
+    </>
+)}
+
+export default Layout
