7d9710780b5cd6c7192ef99d7211f4d68c19eb78
Greque <ichijou_raku@hotmail.com>
2025-08-06 01:25:11 -0300

    redirects

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,25 @@
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate
+} from "react-router-dom";
+
+import ProductDetails from './sections/pages/ProductDetails.jsx';
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<App/>}></Route>
+        <Route exact path="/details" element={<ProductDetails/>}></Route>
+      </Routes>
+    </Router>
   </StrictMode>,
 )
---
+++ client/src/sections/pages/ProductDetails.jsx
@@ -0,0 +1,11 @@
+import Layout from '../layout/Layout.jsx';
+
+function ProductDetails() {
+  return (
+    <Layout>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, suscipit. Dolorem, hic. Nihil consequatur, molestias eius tempore ea, voluptate maiores magnam placeat fugit officiis enim consequuntur. Accusantium, possimus commodi asperiores modi, eveniet dignissimos placeat sapiente ipsum natus fugit sunt similique mollitia, atque ex animi eum? Tempore temporibus perspiciatis quisquam voluptas inventore, consectetur eaque ex accusantium accusamus nostrum numquam, ut eos odit, distinctio praesentium soluta quas quae nemo tenetur dolorum aperiam pariatur animi! Minima iste totam quos magni, non consequatur, dolor beatae fugiat aliquam quae cum! Officiis suscipit minus rerum ea incidunt, recusandae alias quidem accusamus vero eligendi repellendus, dolorum corrupti eos aliquid illo! Nemo molestiae cum ea suscipit minus consectetur magni assumenda nulla enim libero, fugiat cumque tempore, quis quia rerum? Tempore praesentium quos neque. Dolor, harum autem possimus voluptate repudiandae perspiciatis corrupti nostrum obcaecati, aut cupiditate voluptates sunt quo eaque ipsam. Eius, quas obcaecati itaque molestiae, aut culpa perspiciatis, nam repellat minima libero quia delectus molestias neque dolorum ipsum vitae officia et autem qui iure nesciunt consectetur! Id quos eum maxime similique sit autem possimus a aliquid, saepe ullam quas minima fuga sint atque pariatur non eius velit voluptate adipisci. Sed unde tenetur temporibus, dolorem ipsum facere vel nobis itaque, hic suscipit maxime dolore odit inventore, eos atque harum iure voluptas doloribus quam impedit? Omnis nesciunt sunt, minima voluptas molestias maiores delectus! Nemo animi, nesciunt voluptatum, dolore ducimus eius error quos molestias, ipsam eum quidem. Voluptates unde nostrum consectetur doloribus cumque. Earum.
+    </Layout>
+  )
+}
+
+export default ProductDetails
